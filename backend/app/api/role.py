04b5fbe7f9226ef9f3ceec1f0ef97316da52b08f
Xiaoxunyun <149136428+Xiaoxunyun@users.noreply.github.com>
2025-09-25 22:29:58 +0800

    feat:部分返回response

--- backend/app/api/role.py
+++ backend/app/api/role.py
@@ -1,5 +1,13 @@
+from fastapi import APIRouter
+
+router = APIRouter()
 # POST   /roles        # 创建角色
+# @router.post("/")
+# def create_role(role:dict)
+
 # GET    /roles        # 获取角色列表
+@router.get("/")
+def get_roles()
 # GET    /roles/{id}   # 获取单个角色详情
 # PUT    /roles/{id}   # 更新角色
 # DELETE /roles/{id}   # 删除角色
