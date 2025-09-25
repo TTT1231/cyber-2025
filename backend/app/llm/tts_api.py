513580c3eeb34520fd1acb953222a00cdbc84881
ZDY <948987619@qq.com>
2025-09-26 03:50:02 +0800

    新增语音识别+语音合成

--- backend/app/llm/tts_api.py
+++ backend/app/llm/tts_api.py
@@ -1,34 +1,26 @@
-
-
-
 #  DashScope SDK 版本不低于 1.24.6
-# import os
-# import requests
-# import dashscope
-# text = "那我来给大家推荐一款T恤，这款呢真的是超级好看，这个颜色呢很显气质，而且呢也是搭配的绝佳单品，大家可以闭眼入，真的是非常好看，对身材的包容性也很好，不管啥身材的宝宝呢，穿上去都是很好看的。推荐宝宝们下单哦。"
-# # SpeechSynthesizer接口使用方法：dashscope.audio.qwen_tts.SpeechSynthesizer.call(...)
-# response = dashscope.MultiModalConversation.call(
-#     model="qwen3-tts-flash",
-#     api_key=os.getenv("API_KEY"),#os.getenv("DASHSCOPE_API_KEY"),
-#     text=text,
-#     voice="Cherry",
-#     language_type="Chinese",  # 建议与文本语种一致，以获得正确的发音和自然的语调。
-#     stream=False
-# )
-# audio_url = response.output.audio.url
-# save_path = "downloaded_audio.wav"  # 自定义保存路径
-#
-# try:
-#     response = requests.get(audio_url)
-#     response.raise_for_status()  # 检查请求是否成功
-#     with open(save_path, 'wb') as f:
-#         f.write(response.content)
-#     print(f"音频文件已保存至：{save_path}")
-# except Exception as e:
-#     print(f"下载失败：{str(e)}")
+import os
+import dashscope
 
 class TTS():
-    def __init__(self):
-        pass
+    def __init__(self, voice: str, language: str):
+        self.voice = voice
+        self.language = language
+    def generate_audio(self,text:str):
+        response = dashscope.MultiModalConversation.call(
+            model="qwen3-tts-flash",
+            api_key=os.getenv("API_KEY"),
+            text=text,
+            voice="Cherry",
+            language_type="Chinese",  # 建议与文本语种一致，以获得正确的发音和自然的语调。
+            stream=False
+        )
+
+        return response.output.audio.url    # 返回音频url
+
+
+tts = TTS('Cherry','Chinese')
+print(tts.generate_audio('你好，我是哈利波特'))
+
 
 
